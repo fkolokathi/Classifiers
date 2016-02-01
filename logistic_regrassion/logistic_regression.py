ad7a15c47e557fb7e7566d1f5e4d0f2071a9fe41
NightmareNyx <nightmare7777777@gmail.com>
2016-02-01 12:00:04 +0200

    just a reformat

--- logistic_regrassion/logistic_regression.py
+++ logistic_regrassion/logistic_regression.py
@@ -1,6 +1,7 @@
 import math
-class Logistic_Regression(object):
 
+
+class Logistic_Regression(object):
     # constants
 
     # sta8era gia ton periorismo twn varwn w
@@ -12,7 +13,8 @@ class Logistic_Regression(object):
     loads a csv file as a dataset.
     the file consists of number values, either integer of floating point, representing each feature
     '''
-    def load_dataset(self,filename):
+
+    def load_dataset(self, filename):
         f = open(filename, 'r')
         lines = f.readlines()
         f.close()
@@ -26,11 +28,11 @@ class Logistic_Regression(object):
         # parallila dimiourgw kai t dianismata twn paradeigmatwn.
         dataset = []
         for line in lines:
-            attributes = line.split(sep = ',')
+            attributes = line.split(sep=',')
             data = []
             for a in attributes:
                 a.strip()
-                a = float(a) if '.' in a else int(a) # euxomai na kanei apla tn douleia tou <3
+                a = float(a) if '.' in a else int(a)  # euxomai na kanei apla tn douleia tou <3
                 data.append(a)
 
             dataset.append(data)
@@ -44,11 +46,12 @@ class Logistic_Regression(object):
     ---> w[0] is always multiplied by 1
     ---> x[-1] is the response and therefore is not used for the calculations
     '''
+
     def product(self, w, x):
 
         prod = w[0]
         for i in range(len(x)):
-            prod += x[i]*w[i+1]
+            prod += x[i] * w[i + 1]
 
         return prod
 
@@ -58,31 +61,32 @@ class Logistic_Regression(object):
         dataset = self.load_dataset(filename)
 
         num_of_features = len(dataset[0])
-        w = [1/num_of_features]*(num_of_features + 1)
-        k = 0.1 # prepei na ginei ki auto sta8era
-        n = 0.01 # prepe na to valw ws sta8era e3w apo tn sinartisi
+        w = [1 / num_of_features] * (num_of_features + 1)
+        k = 0.1  # prepei na ginei ki auto sta8era
+        n = 0.01  # prepe na to valw ws sta8era e3w apo tn sinartisi
 
         l = 0
 
-        while l-s < 0:
+        while l - s < 0:
             l = s
             self.w = w
             s = 0
-            for  x in dataset:
+            for x in dataset:
 
                 if x[-1] == 1:
-                    s += math.log2(self.positive(self.product(w,x)))
+                    s += math.log2(self.positive(self.product(w, x)))
                 else:
-                    s+= math.log2(self.negative(self.product(w,x)))
+                    s += math.log2(self.negative(self.product(w, x)))
 
-                w[0] += n*(x[-1] - self.positive(self.product(w,x)) -2*k*w[0])
-                for i in range(1,len(w)):
-                    w[i] += n*(x[i]*(x[-1] - self.positive(self.product(w,x))) -2*k*w[i])
+                w[0] += n * (x[-1] - self.positive(self.product(w, x)) - 2 * k * w[0])
+                for i in range(1, len(w)):
+                    w[i] += n * (x[i] * (x[-1] - self.positive(self.product(w, x))) - 2 * k * w[i])
 
-# Prosoxi!!! eite to dianisma x  periexei eite oxi to pedio ts swstis apokrisis to product(w,x) to agnoei
+                    # Prosoxi!!! eite to dianisma x  periexei eite oxi to pedio ts swstis apokrisis to product(w,x) to agnoei
+
     @staticmethod
     def response(self, x):
-        if self.positive(self.product(self.w, x)) >= 0.5: # kata simvasi to 0.5 to 8ewrw entos twn oriwn
+        if self.positive(self.product(self.w, x)) >= 0.5:  # kata simvasi to 0.5 to 8ewrw entos twn oriwn
             return 1
         else:
             return 0
@@ -97,12 +101,10 @@ class Logistic_Regression(object):
         return responses
 
     def positive(self, k):
-        return 1.0/(1.0+math.exp(-1.0*k))
+        return 1.0 / (1.0 + math.exp(-1.0 * k))
 
     def negative(self, k):
-        return math.exp(-1.0*k)/(1.0+math.exp(-1.0*k))
-
-
+        return math.exp(-1.0 * k) / (1.0 + math.exp(-1.0 * k))
 
 
 def training_results():
@@ -111,8 +113,6 @@ def training_results():
 
     test_data_file = ''
     responses = Logistic_Regression.response_all(test_data_file)
-
-
 
 
 if __name__ == "__main__":
