3a0d1c35ba0fb9805b7d1331e520984d5c229604
NightmareNyx <nightmare7777777@gmail.com>
2016-02-02 02:21:14 +0200

    adaboost something

--- adaboost/adaboost.py
+++ adaboost/adaboost.py
@@ -1,32 +1,33 @@
 import math
 
 
-def adaboost(examples, L, M):
-    # examples: a tuple of xi training examples with the proper yi response
+def adaboost(dataset, L, M):
+    # dataset: tuples of xi training examples with the proper yi response
     # L: basic learning algorithm
     # M: number of iterations
     w = []  # vector of the weights of examples
-    N = len(examples)
-    for i in range(N):  # initiliaze all the weights as 1/N
-        w.append(1. / N)
+    N = len(dataset.get_examples)
+    w = [1. / N] * N  # initiliaze all the weights as 1/N
     h = []  # vector to insert M hypotheses we learn
     z = []  # vector to insert the weights of the M hypotheses
 
     # algorithm
     for m in range(1, M):
-        h[m] = L(examples, w)  # learn a new hypothesis (new commission member)
+        h[m] = L(dataset, w)  # learn a new hypothesis (new commission member)
         error = 0
         for j in range(N):
             # calculate the total error of the new hypothesis
             # the sum of w weights of the examples is 1
-            if h[m].get_response(examples[j][0]) != examples[j][1]:
+            if h[m].predict(dataset[j]) != dataset[j]:
                 error += w[j]
         for j in range(N):
             # for error < 0.5, the weight of the correctly classified examples decreases
-            if h[m].get_response(examples[j][0]) == examples[j][1]:
-                w[j] = w[j] * error / (1 - error)
+            if h[m].predict(dataset[j]) == dataset[j][1]:
+                if error == 1:  # avoid division by 0
+                    error += 0.000000001
+                w[j] *= error / (1. - error)
         w = normalize(w)  # so that the sum is 1 again
-        z[m] = math.log((1 - error) / error)  # give more weight to the good hypothesis
+        z[m] = math.log((1. - error) / error)  # give more weight to the good hypothesis
     # this log is a natural log
     return weighted_majority(h, z)
 
@@ -40,10 +41,12 @@ def normalize(weights):
 
 
 def weighted_majority(hypos, weights):
-    views = [h * w for h, w in zip(hypos, weights)]  # a vector of votes for each hypothesis
-    return max(views)
+    views = dict()
+    for v, w in zip(hypos, weights):
+        views[v] += w
+    return max(views.keys(), key=views.get)
 
 
 class Hypothesis:
-    def get_response(example):
+    def predict(example):
         pass
