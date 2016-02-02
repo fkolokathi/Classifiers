b99590230afea2b97972b14dc2eeb5acd3d8b7e0
NightmareNyx <nightmare7777777@gmail.com>
2016-02-02 19:06:40 +0200

    files

--- ID3/Id3_train.py
+++ ID3/Id3_train.py
@@ -16,7 +16,7 @@ class Node:
         self.value = v
 
     def genChildren(self, dictionary):
-        if isinstance(dictionary, dict):
+        if (isinstance(dictionary, dict)):
             self.children = list(dictionary.keys())
 
 
@@ -26,15 +26,14 @@ def train():
     data = []
     for line in file:
         line = line.strip("\r\n")
-        data.append(line.split(','))
-        # to data periexei upolistes opou h kathemia periexei ta values kathe grammhs tou file
-    attr_names = data[0]
-    # H prwth grammh tou file periexei ta onomata twn attributes.
-    # ---------->Ta exw ektypwsei pairnei ta onomata twn attributes mia xara
-    data.remove(attr_names)
-    # afairesh ths prwths grammhs pou periexei ta onomata twn attiributes mias kai den tha xreiastoun kata to training
+        data.append(
+            line.split(','))  # to data periexei upolistes opou h kathemia periexei ta values kathe grammhs tou file
+    attributes_names = data[
+        0]  # H prwth grammh tou file periexei ta onomata twn attributes.---------->Ta exw ektypwsei pairnei ta onomata twn attributes mia xara
+    data.remove(
+        attributes_names)  # afairesh ths prwths grammhs pou periexei ta onomata twn attiributes mias kai den tha xreiastoun kata to training
     # Run ID3
-    tree = Id3.id3(data, attr_names, goal)
+    tree = Id3.id3(data, attributes_names, goal)
     return tree
 
 
@@ -46,8 +45,8 @@ def attributes_names():  # ------->Kai edw swsta tha ta pairnei.einai akrivws to
         line = line.strip("\r\n")
         data.append(
             line.split(','))  # to data periexei upolistes opou h kathemia periexei ta values kathe grammhs tou file
-    attr_names = data[0]
-    return attr_names
+    attributes_names = data[0]
+    return attributes_names
 
 
 def test():
@@ -62,11 +61,11 @@ def test():
             (line.split(',')))  # to data periexei upolistes opou h kathemia periexei ta values kathe grammhs tou file
     ###########################################
     count = 0
-    for entry in data:
+   for entry in data:
         count += 1
         dictionary = tree.copy()
         result = ""
-        while isinstance(dictionary, dict):
+        while (isinstance(dictionary, dict)):
             root = Node(list(dictionary.keys())[0], dictionary[list(dictionary.keys())[0]])
             dictionary = dictionary[list(dictionary.keys())[0]]
             index = attributes.index(root.value)
@@ -81,13 +80,12 @@ def test():
                 break
         # print ("entry%s = %s" % (count, result))
         lis = entry
-        lis.pop()  # the last element of list entry is " " because of the last comma that every tuple
-        # has (sunny,cool,high,TRUE,) in weather.csv file
+        lis.pop()  #the last element of list entry is " " because of the last comma that every tuple has (sunny,cool,high,TRUE,) in weather.csv file
         lis.append(result)
-        length = len(lis) - 1
-        i = 0
+        length=len(lis) - 1
+        i= 0
         for item in lis:
-            if i == 0:
+            if i==0:
                 o.write(item)
                 i = i + 1
             elif i != 0 and i != length:
@@ -96,4 +94,4 @@ def test():
             else:  # last item(response)
                 o.write(',' + item)
         o.write('\n')
-    o.close()
+   o.close()
